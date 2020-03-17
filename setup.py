venkuppu-chn/cortx-py-utils

HA_Generator: Added logic for provision
- Added args paramenter
- Moved all schema to Doc

setup.py
@@ -32,11 +32,12 @@ setup(name='eos-py-utils',
       author_email='alexander.voronov@seagate.com',
       description='Common Python utilities for EOS',
       package_dir={'eos': 'src'},
-      packages=['eos', 'eos.utils', 'eos.utils.db', 'eos.utils.security'],
+      packages=['eos', 'eos.utils', 'eos.utils.db', 'eos.utils.security', 'eos.utils.conf'],
       package_data={
         'eos': ['py.typed'],
       },
       long_description=long_description,
       zip_safe=False,
       python_requires='>=3.6.8',
-      install_requires=['cryptography==2.8', 'schematics==2.1.0'],)
+      install_requires=['cryptography==2.8', 'schematics==2.1.0', 'toml==0.10.0',
+                        'PyYAML==5.1.2', 'configparser==4.0.2'])

src/utils/ha/generate.py
@@ -20,23 +20,58 @@
 import os
 import json
 import traceback
+import re
+import ast
 from string import Template
 
+from eos.utils.conf.conf import Conf
+from eos.utils.conf.payload import *
+from ha import const
 
-#TODO: add logic for $variable for dynamic input in args_file
 class Generator:
     def __init__(self, compiled_file, output_file, args_file):
-        if not os.path.isfile(compiled_file):
-            raise Exception("%s invalid file in genarator" %compiled_file)
+        """
+        compiled_file   : Compiled file generate by hac compiler
+        output_file     : Output file for target ha tool
+        args_file       : Provision file for dynamic input
+        """
+        Conf.init()
+        if compiled_file is None:
+            raise Exception("compiled_file is missing")
         if output_file is None:
-            self._script = "/tmp/output_script.sh"
-        elif os.path.isdir(output_file):
-            self._script = output_file + "output_script.sh"
-        else:
-            self._script = output_file
+            raise Exception("output_file is missing")
+        if args_file is None:
+            raise Exception("args_file is missing")
+        self._is_file(compiled_file)
+        self._is_file(output_file)
+        self._is_file(args_file)
+        Conf.load(const.PROV_CONF_INDEX, Yaml(args_file))
+        self._script = output_file
         with open(compiled_file, "r") as f:
             self.compiled_json = json.load(f)
+            self._provision_compiled_schema(self.compiled_json)
         self._resource_set = self.compiled_json["resources"]
+
+    def _provision_compiled_schema(self, compiled_schema):
+        """
+        Scan schema and replace ${var} in compiled schema
+        to configuration provided by provision.
+        """
+        keys = re.findall(r"\${[^}]+}(?=[^]*[^]*)", str(compiled_schema))
+        args = {}
+        new_compiled_schema = str(compiled_schema)
+        for element in keys:
+            key = element.replace("${", "").replace("}", "")
+            new_compiled_schema = new_compiled_schema.replace(element,
+                    Conf.get(const.PROV_CONF_INDEX, key, element))
+        self.compiled_json = ast.literal_eval(new_compiled_schema)
+
+    def _is_file(self, filename):
+        """
+        Check if file exists
+        """
+        if not os.path.isfile(filename):
+            raise Exception("%s invalid file in genarator" %filename)
 
     def _cluster_create(self):
         pass

src/utils/conf/conf.py
@@ -1,0 +1,97 @@
+#!/usr/bin/env python3
+
+"""
+ ****************************************************************************
+ Filename:          conf.py
+ Description:       Contains the configuration handling for CSM
+
+ Creation Date:     31/05/2018
+ Author:            Ujjwal Lanjewar
+
+ Do NOT modify or remove this copyright and confidentiality notice!
+ Copyright (c) 2001 - $Date: 2015/01/14 $ Seagate Technology, LLC.
+ The code contained herein is CONFIDENTIAL to Seagate Technology, LLC.
+ Portions are also trade secret. Any use, duplication, derivation, distribution
+ or disclosure of this code, for any reason, not expressly authorized is
+ prohibited. All other rights are expressly reserved by Seagate Technology, LLC.
+ ****************************************************************************
+"""
+import os
+from eos.utils.conf.payload import *
+
+class Conf:
+    ''' Represents conf file - singleton '''
+    _payloads = {}
+
+    @staticmethod
+    def init():
+        ''' Initializes data from conf file '''
+        pass
+
+    @staticmethod
+    def load(index, doc, force=False):
+        if not os.path.isfile('%s' %doc):
+            raise Exception(-1, 'File %s does not exist' %doc)
+        if index in Conf._payloads.keys():
+            if force == False:
+                raise Exception('index %s is already loaded')
+            Conf.save(index)
+        Conf._payloads[index] = Payload(doc)
+
+    @staticmethod
+    def get(index, key, default_val=""):
+        ''' Obtain value for the given key '''
+        return Conf._payloads[index].get(key) if Conf._payloads[index].get(key) is not None else default_val
+
+    @staticmethod
+    def set(index, key, val):
+        ''' Sets the value into the conf for the given key '''
+        Conf._payloads[index].set(key, val)
+
+    @staticmethod
+    def save(index=None):
+        indexes = [x for x in _payloads.keys()] if index is None else index
+        for index in indexes:
+            Conf._payloads[index].dump()
+
+
+class ConfSection:
+    """Represents sub-section of config file"""
+
+    def __init__(self, from_dict: dict):
+        """
+        Initialize ConfSection by dictionary object
+
+        :param dict from_dict: base dictionary to create object from its keys and values
+        """
+        for key, value in from_dict.items():
+            if isinstance(value, dict):
+                setattr(self, key, ConfSection(value))
+            else:
+                setattr(self, key, value)
+
+
+class DebugConf:
+    """
+    Class which simplifies work with debug settings in debug mode:
+
+    make easy check whether debug-mode is enabled and requested option is set
+    to desired value
+    """
+
+    def __init__(self, debug_settings: ConfSection):
+        """
+        Initialize debug configuration instance by debug settings
+
+        """
+        self._debug_settings = debug_settings
+
+    def __getattr__(self, attr):
+        return getattr(self._debug_settings, attr)
+
+    @property
+    def http_enabled(self):
+        """
+        Validates if debug mode is enabled and HTTP is chosen
+        """
+        return self._debug_settings.enabled and self._debug_settings.http_enabled

ypise/cortx-utils

setup.py
@@ -32,11 +32,12 @@ setup(name='eos-py-utils',
       author_email='alexander.voronov@seagate.com',
       description='Common Python utilities for EOS',
       package_dir={'eos': 'src'},
-      packages=['eos', 'eos.utils', 'eos.utils.db', 'eos.utils.security'],
+      packages=['eos', 'eos.utils', 'eos.utils.db', 'eos.utils.security', 'eos.utils.schema'],
       package_data={
         'eos': ['py.typed'],
       },
       long_description=long_description,
       zip_safe=False,
       python_requires='>=3.6.8',
-      install_requires=['cryptography==2.8', 'schematics==2.1.0'],)
+      install_requires=['cryptography==2.8', 'schematics==2.1.0', 'toml==0.10.0',
+                        'PyYAML==5.1.2', 'configparser==4.0.2'])

src/utils/ha/generate.py
@@ -20,23 +20,57 @@
 import os
 import json
 import traceback
+import re
+import ast
 from string import Template
 
+from eos.utils.schema.conf import Conf
+from eos.utils.schema.payload import *
+from ha import const
 
-#TODO: add logic for $variable for dynamic input in args_file
 class Generator:
     def __init__(self, compiled_file, output_file, args_file):
-        if not os.path.isfile(compiled_file):
-            raise Exception("%s invalid file in genarator" %compiled_file)
+        """
+        compiled_file   : Compiled file generate by hac compiler
+        output_file     : Output file for target ha tool
+        args_file       : Provision file for dynamic input
+        """
+        if compiled_file is None:
+            raise Exception("compiled_file is missing")
         if output_file is None:
-            self._script = "/tmp/output_script.sh"
-        elif os.path.isdir(output_file):
-            self._script = output_file + "output_script.sh"
-        else:
-            self._script = output_file
+            raise Exception("output_file is missing")
+        if args_file is None:
+            raise Exception("args_file is missing")
+        self._is_file(compiled_file)
+        self._is_file(output_file)
+        self._is_file(args_file)
+        Conf.load(const.PROV_CONF_INDEX, Yaml(args_file))
+        self._script = output_file
         with open(compiled_file, "r") as f:
             self.compiled_json = json.load(f)
+            self._provision_compiled_schema(self.compiled_json)
         self._resource_set = self.compiled_json["resources"]
+
+    def _provision_compiled_schema(self, compiled_schema):
+        """
+        Scan schema and replace ${var} in compiled schema
+        to configuration provided by provision.
+        """
+        keys = re.findall(r"\${[^}]+}(?=[^]*[^]*)", str(compiled_schema))
+        args = {}
+        new_compiled_schema = str(compiled_schema)
+        for element in keys:
+            key = element.replace("${", "").replace("}", "")
+            new_compiled_schema = new_compiled_schema.replace(element,
+                    Conf.get(const.PROV_CONF_INDEX, key, element))
+        self.compiled_json = ast.literal_eval(new_compiled_schema)
+
+    def _is_file(self, filename):
+        """
+        Check if file exists
+        """
+        if not os.path.isfile(filename):
+            raise Exception("%s invalid file in genarator" %filename)
 
     def _cluster_create(self):
         pass

src/utils/schema/conf.py
@@ -1,0 +1,97 @@
+#!/usr/bin/env python3
+
+"""
+ ****************************************************************************
+ Filename:          conf.py
+ Description:       Contains the configuration handling for CSM
+
+ Creation Date:     31/05/2018
+ Author:            Ujjwal Lanjewar
+
+ Do NOT modify or remove this copyright and confidentiality notice!
+ Copyright (c) 2001 - $Date: 2015/01/14 $ Seagate Technology, LLC.
+ The code contained herein is CONFIDENTIAL to Seagate Technology, LLC.
+ Portions are also trade secret. Any use, duplication, derivation, distribution
+ or disclosure of this code, for any reason, not expressly authorized is
+ prohibited. All other rights are expressly reserved by Seagate Technology, LLC.
+ ****************************************************************************
+"""
+import os
+from eos.utils.schema.payload import *
+
+class Conf:
+    ''' Represents conf file - singleton '''
+    _payloads = {}
+
+    @staticmethod
+    def init():
+        ''' Initializes data from conf file '''
+        pass
+
+    @staticmethod
+    def load(index, doc, force=False):
+        if not os.path.isfile('%s' %doc):
+            raise Exception(-1, 'File %s does not exist' %doc)
+        if index in Conf._payloads.keys():
+            if force == False:
+                raise Exception('index %s is already loaded')
+            Conf.save(index)
+        Conf._payloads[index] = Payload(doc)
+
+    @staticmethod
+    def get(index, key, default_val=""):
+        ''' Obtain value for the given key '''
+        return Conf._payloads[index].get(key) if Conf._payloads[index].get(key) is not None else default_val
+
+    @staticmethod
+    def set(index, key, val):
+        ''' Sets the value into the conf for the given key '''
+        Conf._payloads[index].set(key, val)
+
+    @staticmethod
+    def save(index=None):
+        indexes = [x for x in _payloads.keys()] if index is None else index
+        for index in indexes:
+            Conf._payloads[index].dump()
+
+
+class ConfSection:
+    """Represents sub-section of config file"""
+
+    def __init__(self, from_dict: dict):
+        """
+        Initialize ConfSection by dictionary object
+
+        :param dict from_dict: base dictionary to create object from its keys and values
+        """
+        for key, value in from_dict.items():
+            if isinstance(value, dict):
+                setattr(self, key, ConfSection(value))
+            else:
+                setattr(self, key, value)
+
+
+class DebugConf:
+    """
+    Class which simplifies work with debug settings in debug mode:
+
+    make easy check whether debug-mode is enabled and requested option is set
+    to desired value
+    """
+
+    def __init__(self, debug_settings: ConfSection):
+        """
+        Initialize debug configuration instance by debug settings
+
+        """
+        self._debug_settings = debug_settings
+
+    def __getattr__(self, attr):
+        return getattr(self._debug_settings, attr)
+
+    @property
+    def http_enabled(self):
+        """
+        Validates if debug mode is enabled and HTTP is chosen
+        """
+        return self._debug_settings.enabled and self._debug_settings.http_enabled